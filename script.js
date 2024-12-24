def54a6c886f10c850f5150cdb35a88b4b7234c0
Sofya Dovgopolova <71345115+OdiumAeternum@users.noreply.github.com>
2024-12-25 00:33:32 +0300

    Updated with BEM

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 $(function() {
     $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
-            $('#back-to-top').fadeIn();
+            $('.back-to-top').fadeIn();
         } else {
-            $('#back-to-top').fadeOut();
+            $('.back-to-top').fadeOut();
         }
     });
     
-    $('#back-to-top').click(function() {
-        $(this).addClass('rotating');
+    $('.back-to-top').click(function() {
+        $(this).addClass('back-to-top--rotating');
         $('body,html').animate({scrollTop:0}, 700);
         
         setTimeout(() => {
-            $(this).removeClass('rotating');
+            $(this).removeClass('back-to-top--rotating');
         }, 2000);
     });
 });
 
-
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".design__slider", {
     spaceBetween: 30,
     navigation: {
         nextEl: ".swiper-button-next",
